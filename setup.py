14d0e878672713545c1148b95794a4a8982bc935
Parthiv Akilesh <parthivakilesh@gmail.com>
2023-02-21 21:27:15 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its about making a wafer project using MLOPS',
+    author='asdfg',
+    license='MIT',
+)
